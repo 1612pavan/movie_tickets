e9287e61d553b4fe992648341d0aa460b767b352
1612pavan <162822893+1612pavan@users.noreply.github.com>
2024-12-23 09:54:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import sys
+import book_tickets as bt
+
+
+
+arg1 = sys.argv[1] # [file_name, arg1, arg2]
+
+if len(sys.argv) >= 3:
+    arg2 = sys.argv[2]
+
+if arg1 == "movie" and arg2 == "list":
+    bt.get_movie_list()
+
+elif arg1 == "book":
+    # Book Ticket for Specific Movie
+    no_of_tickets = int(input("No of tickets: "))
+    bk_name = input("Booking Name: ")
+    bt.get_ticket(arg2, no_of_tickets, bk_name)
+
+elif arg1 == "--help":
+    # Show help menu
+    print("-------------Help Menu---------------\nCommands:")
+    print("1. movie list")
+    print("2. book {movie_name}")
+    print("----------------------------")
